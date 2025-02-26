a90519fbe76ace3bf30a6bc3fd90b0fc1b645771
Alex O'Shea (MBA) <bras5436@ox.ac.uk>
2025-02-26 11:47:40 +0000

    plotting and animation methods

--- ising_mcmc_v2.py
+++ ising_mcmc_v2.py
@@ -7,13 +7,16 @@ import numpy.typing as npt
 import os
 import logging
 import pickle
+import matplotlib.pyplot as plt
 
 
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict, List, Tuple
 from datetime import timedelta
+from matplotlib.animation import FuncAnimation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 # %%
@@ -22,7 +25,7 @@ from datetime import timedelta
 @dataclass(frozen=True)
 class MonteCarloResult:
 
-    results: List[npt.NDArray[np.int_]]
+    results: npt.NDArray[np.int_]
     lattice_size: float
     temp: float
     beta: float
@@ -31,6 +34,52 @@ class MonteCarloResult:
     eq_steps: int
     sim_steps: int
     flip_frac: float
+
+    def show_lattice(
+        self,
+        step: int,
+        cmap: str = "viridis",
+        figsize: Tuple[float, float] = (8, 8),
+        dpi: int = 300,
+    ) -> Tuple[plt.Figure, plt.Axes, plt.Colorbar]:
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+        im = ax.imshow(self.results[step], cmap=cmap, vmin=-1, vmax=1, origin="lower")
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size=f"{5}%", pad=0.2)
+        cbar = plt.colorbar(im, cax=cax, orientation="vertical")
+
+        return fig, ax, cbar
+
+    def animate_lattice(
+        self,
+        filename: str,
+        fps: int = 200,
+        cmap: str = "viridis",
+        figsize: Tuple[float, float] = (8, 8),
+        dpi: int = 200,  # animation scales badly with dpi
+    ):
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+        im = ax.imshow(self.results[0], cmap=cmap, vmin=-1, vmax=1, origin="lower")
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size=f"{5}%", pad=0.2)
+        cbar = plt.colorbar(im, cax=cax, orientation="vertical")
+        title = ax.set_title(f"Step 0")
+
+        def update(step):
+            im.set_array(self.results[step + 1])
+            title.set_text(f"Step {step + 1}")
+
+            return im, title
+
+        anim = FuncAnimation(fig, update, frames=len(self.results) - 1, blit=True)
+
+        anim.save(
+            f"{filename}.mp4",
+            writer="ffmpeg",  # may need to install this and tell matplotlib where the writer path is
+            fps=fps,
+        )
 
 
 def calc_energy(lattice: np.ndarray, J: float = 1.0) -> float:
